1f5615374eae6639890a7cdf2cc6bf5a2d0add7c
zahoov <53627016+zahoov@users.noreply.github.com>
2021-07-15 13:43:41 -0700

    Update pi_logic.py

--- pi_logic.py
+++ pi_logic.py
@@ -2,15 +2,13 @@ import subprocess
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-#GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
-#GPIO.setup(26, GPIO.IN)    # set GPIO25 as input (button)
 GPIO.setup(26, GPIO.IN, GPIO.PUD_DOWN)
 
-# wait for the pin to be sorted with GND and, if so, halt the system
-#GPIO.wait_for_edge(26, GPIO.FALLING)
 while True:
         if GPIO.input(26):
+                
+                #INSERT WHATEVER CODE YOU WANT TO RUN BEFORE SHUTDOWN HERE, THE NEXT LINE SHUTS OFF THE PI
                 subprocess.call(['shutdown -h -P now "System halted by GPIO action"'], shell=True)
                 exit()
-# clean up GPIO on normal exit
+
 GPIO.cleanup()
